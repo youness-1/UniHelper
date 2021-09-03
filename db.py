fc5adbd7af0a0409ea59f4fdd64c67102e1ae7b1
youness-1 <31848743+youness-1@users.noreply.github.com>
2021-09-03 18:31:17 +0200

    Add files via upload

--- db.py
+++ db.py
@@ -7,13 +7,13 @@ def main():
     c.execute('''CREATE TABLE CorsoLaurea
                 ([corso_laurea] text,[dipartimento] text,[classe] text, PRIMARY KEY (corso_laurea))''')
     c.execute('''CREATE TABLE Lezione
-                ([insegnamento] text,[docenti] text, [corso_laurea] text, [link] text, PRIMARY KEY (insegnamento))''')
+                ([insegnamento] text,[docenti] text, [corso_laurea] text, [link] text, PRIMARY KEY (insegnamento), FOREIGN KEY (corso_laurea) REFERENCES CorsoLaurea (corso_laurea))''')
     c.execute('''CREATE TABLE Utente
                 ([id_utente] text,[ultimo_comando] integer,[corso_laurea] text, PRIMARY KEY (id_utente), FOREIGN KEY (corso_laurea) REFERENCES CorsoLaurea (corso_laurea))''')
     c.execute('''CREATE TABLE Esame
-                ([insegnamento] text,[docenti] text,[corso_laurea] text,[data_orario] text, [link] text, PRIMARY KEY (insegnamento))''')
+                ([insegnamento] text,[docenti] text,[corso_laurea] text,[data_orario] text, [link] text, PRIMARY KEY (insegnamento), FOREIGN KEY (corso_laurea) REFERENCES CorsoLaurea (corso_laurea))''')
     c.execute('''CREATE TABLE Laurea
-                ([dipartimento] text,[corso_laurea] text,[data_orario] text,[codice] text, PRIMARY KEY (data_orario, codice))''')    
+                ([dipartimento] text,[corso_laurea] text,[data_orario] text,[codice] text, PRIMARY KEY (data_orario, codice), FOREIGN KEY (corso_laurea) REFERENCES CorsoLaurea (corso_laurea))''')    
     conn.commit()
     conn.close()
 
